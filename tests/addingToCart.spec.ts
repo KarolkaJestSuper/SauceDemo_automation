f1b26270b07e34d5740f35cf3b8f3e762f1c62d7
KarolkaJestSuper <karo.kozlovska@gmail.com>
2025-06-13 19:05:49 +0800

    added beforeEach hooks
    added beforeEach hooks
    moved url to the  m playwright.config file

--- tests/addingToCart.spec.ts
+++ tests/addingToCart.spec.ts
@@ -1,15 +1,18 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Adding to cart tests', () => {
-// Arrange
-  const url = 'https://www.saucedemo.com/';
-  const userName = 'standard_user';
-  const userPass = 'secret_sauce';
-// Act
-  test('listing all items from the main page', async ({ page }) => {
-    await page.goto(url);
+  
+  // Arrange
+  test.beforeEach(async ({ page }) => {
+    const userName = 'standard_user';
+    const userPass = 'secret_sauce';
+    await page.goto('/');
     await page.locator('#user-name').fill(userName);
     await page.locator('#password').fill(userPass);
+  });
+  
+// Act
+  test('listing all items from the main page', async ({ page }) => {
     await page.locator('#login-button').click();
 
 // Assert
@@ -19,9 +22,6 @@ test.describe('Adding to cart tests', () => {
 // Arrange
 // Act
   test('adding an item to the cart', async ({ page }) => {
-    await page.goto(url);
-    await page.locator('#user-name').fill(userName);
-    await page.locator('#password').fill(userPass);
     await page.locator('#login-button').click();
     await page.locator('#add-to-cart-sauce-labs-backpack').click();
     await page.locator('[data-test="shopping-cart-link"]').click();
@@ -33,29 +33,26 @@ test.describe('Adding to cart tests', () => {
 // Arrange
 // Act
   test('removing from the cart', async ({ page }) => {
-    await page.goto(url);
-    await page.locator('#user-name').fill(userName);
-    await page.locator('#password').fill(userPass);
+
     await page.locator('#login-button').click();
     await page.locator('#add-to-cart-sauce-labs-backpack').click();
     await page.locator('[data-test="shopping-cart-link"]').click();
     await page.locator('[data-test="inventory-item"]').click();
     await page.locator('[data-test="remove-sauce-labs-backpack"]').click();
-    
+
 // Assert
     await expect(page.locator('.cart_quantity')).toHaveCount(0);
-
   });
 
   test('details of the added to cart item', async ({ page }) => {
-    await page.goto(url);
-    await page.locator('#user-name').fill(userName);
-    await page.locator('#password').fill(userPass);
+    // Arrange
+    // Act
     await page.locator('#login-button').click();
     await page.locator('#add-to-cart-sauce-labs-backpack').click();
     await page.locator('[data-test=item-4-title-link]').click();
-    await expect(page.locator('[data-test="inventory-item-name"]')).toHaveText('Sauce Labs Backpack');
 
+// Assert
+    await expect(page.locator('[data-test="inventory-item-name"]')).toHaveText('Sauce Labs Backpack');
   });
  
   });
--- tests/loginPage.spec.ts
+++ tests/loginPage.spec.ts
@@ -2,15 +2,16 @@ import { test, expect } from '@playwright/test';
 import { url } from 'inspector';
 
 test.describe('User login fo sauceDemo', () => {
+  test.beforeEach(async ({ page }) => {
+    // const url = 'https://www.saucedemo.com/';
+    await page.goto('/');
+  });
   //Arrange
-  const url = 'https://www.saucedemo.com/';
   const userId = 'standard_user';
   const userPass = 'secret_sauce';
   const expectedError = 'Epic sadface: Username and password do not match any user in this service';
 //Act
   test('succesfull login with correct creds', async ({ page }) => {
-
-    await page.goto(url);
     await page.locator('#user-name').fill(userId);
     await page.locator('#password').fill(userPass);
     await page.locator('#login-button').click();
@@ -21,7 +22,6 @@ test.describe('User login fo sauceDemo', () => {
 // Arrange
 // Act
   test('unsuccessful login with incorrect password', async ({ page }) => {
-    await page.goto(url);
     await page.locator('#user-name').fill(userId);
     await page.locator('#password').fill('secret_sue');
     await page.locator('#login-button').click();
@@ -29,11 +29,10 @@ test.describe('User login fo sauceDemo', () => {
 // Assert
     await expect (page.locator('[data-test="error"]')).toHaveText(expectedError);
   });
-  
+
 // Arrange
 // Act
   test('unsuccessful login with incorrect username', async ({ page }) => {
-    await page.goto(url);
     await page.locator('#user-name').fill('karo123');
     await page.locator('#password').fill(userPass);
     await page.locator('#login-button').click();
